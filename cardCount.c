150623ae7cc80bdd693370618b8fe263f823fe8f
Олег Комисаренко <olegkomisarenko@MacBook-Pro-Oleg.local>
2021-02-25 22:50:49 +0300

    While has been added to validate card

--- cardCount.c
+++ cardCount.c
@@ -4,26 +4,37 @@
 int	main(void)
 {
 	char card_name[3];
-	
-	puts("Введите название карты: ");
-	scanf("%2s", card_name);
-	int val = 0;
-	switch (card_name[0])
+	int count = 0;
+	while (card_name[0] != 'X')
 	{
-	case 'K':
-	case 'Q':
-	case 'J':
-		val = 10;
-		break;
-	case 'A':
-		val = 11;
-		break;
-	default: 
-		val = atoi(card_name);
-	}
+		puts("Введите название карты: ");
+		scanf("%2s", card_name);
+		int val = 0;
+		switch (card_name[0])
+		{
+		case 'K':
+		case 'Q':
+		case 'J':
+			val = 10;
+			break;
+		case 'A':
+			val = 11;
+			break;
+		case 'X':
+			continue;
+		default: 
+			val = atoi(card_name);
+			if ((val < 1) || (val > 10))
+			{
+				puts("Ошибка карты");
+				continue;
+			}
+		}
 	if ((val >= 3) && (val <= 6))
-		puts("Счетчик увеличился");
+		count++;
 	else if (val == 10)
-		puts("Счетчик уменьшился");
+		count--;
+	printf("Текущий счет: %i\n", count);
+	}
 	return (0);
 }
