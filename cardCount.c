b2bba5b82758077d11dd2bed414e3a96e6886df9
Олег Комисаренко <olegkomisarenko@MacBook-Pro-Oleg.local>
2021-02-25 21:55:39 +0300

    switch added

--- cardCount.c
+++ cardCount.c
@@ -8,18 +8,21 @@ int	main(void)
 	puts("Введите название карты: ");
 	scanf("%2s", card_name);
 	int val = 0;
-	if (card_name[0] == 'K')
+	switch (card_name[0])
 	{
+	case 'K':
 		val = 10;
-	}
-	else if (card_name[0] == 'Q')
+		break;
+	case 'Q':
 		val = 10;
-	else if (card_name[0] == 'J')
+		break;
+	case 'J':
 		val = 10;
-	else if (card_name[0] == 'A')
+		break;
+	case 'A':
 		val = 11;
-	else
-	{
+		break;
+	default: 
 		val = atoi(card_name);
 	}
 	if ((val >= 3) && (val <= 6))
